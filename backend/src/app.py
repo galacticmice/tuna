ccef1117db275126c070626e3215aa06b160aca4
ben <172694952+galacticmice@users.noreply.github.com>
2025-05-12 12:33:02 -0700

    clear up imports

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
-from typing import Union
 import uvicorn
-import json #testing
-
 from .llm import llm_response, parallelize_requests
-from .trends import trend_data
 
 app = FastAPI()
 
@@ -26,10 +22,6 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/test") # rank?
-async def read_item():
-    yield json.dumps({"id": 0, "content": "Hello World"}) + "\n"
-
 @app.get('/get-llm-response/{country}')
 async def get_llm_response(country: str):
     return StreamingResponse(parallelize_requests(country), media_type="application/x-ndjson")
@@ -37,7 +29,3 @@ async def get_llm_response(country: str):
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8080)
-
-# on click, redirect to /geo_code
-# get parameters from url {geo_code} {rank} with urlparser
-# use returned parameters to proc db and LLM operation
