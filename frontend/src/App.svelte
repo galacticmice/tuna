648ca2a51ff83bb436498de889e4b318c1b8c350
anthony <anthonyflores5844@gmail.com>
2025-05-13 00:27:08 -0700

    Added a fish svg favicon

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,6 +4,10 @@
   import Navbar from './lib/Navbar.svelte';
 </script>
 
+<svelte:head>
+  <link rel="icon" type="image/svg+xml" href="/fish.svg" />
+</svelte:head>
+
 <main>
   <Navbar />
   <div class="map">
