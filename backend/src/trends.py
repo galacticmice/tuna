beced1cfbb173cd6df55f50c2ca3dd533ef13a41
anthony <anthonyflores5844@gmail.com>
2025-05-13 14:27:57 -0700

    Added a check for the news[] to prevent out of index

--- backend/src/trends.py
+++ backend/src/trends.py
@@ -6,7 +6,6 @@ tr = Trends()
 
 def trend_data(region: str, i: int, categoryID: int):
     """get trending data for region+rank and return as RegionData object for database
-
         Args:
             region (str): Region code (e.g., 'US-CA')
             i (int): Rank of the trend (0-4)
@@ -15,20 +14,24 @@ def trend_data(region: str, i: int, categoryID: int):
             RegionData: Object containing region code, rank, keyword, and links to articles
     """
     trends = tr.trending_now(geo=region).filter_by_topic(topic=categoryID)
+
     # Added a check if no data is returned
     if not trends or i >= len(trends):
         print(f"No trending data for region {region} at rank {i}.")
         return None
+
     news = tr.trending_now_news_by_ids(
         trends[i].news_tokens,  # News tokens from trending topic
         max_news=3  # Number of articles to retrieve
     )
+
     obj = RegionData(
         region_code=region,
         rank=i,
         keyword=trends[i].keyword,
-        link1=news[0].url,
-        link2=news[1].url,
-        link3=news[2].url
+        # If a news does not exist make it none to prevent out of index error
+        link1=news[0].url if len(news) > 0 else None,
+        link2=news[1].url if len(news) > 1 else None,
+        link3=news[2].url if len(news) > 2 else None
     )
     return obj
